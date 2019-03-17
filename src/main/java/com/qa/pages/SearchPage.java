e3c460006ae2de1b5b4676f50d50d84ab352b5a6
kunal kashyap <iamkunal.automation@gmail.com>
2019-03-17 16:22:16 +0530

    hello

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -7,6 +7,7 @@ public class SearchPage extends TestBase {
 	public void clickOnSaerchButton() {
 		System.out.println("clicked");
 		System.out.println("done");
+                System.out.println("done 1")
 	}
 
 }
